b7fa5906c36842537672086e730a7a231f64c487
trent <tjmcc01@gmail.com>
2017-07-03 17:24:15 -0700

    Adds glfw3 as a dependency

--- main.cpp
+++ main.cpp
@@ -1,10 +1,23 @@
 #include <iostream>
+#include <GLFW/glfw3.h>
+
 #include "emulator.h"
 
 int main() {
+
     std::cout << "Hello, World!" << std::endl;
     emulator *emu;
     emu = new emulator(1024, 1, 1);
     emu->run();
+
+    if (!glfwInit())
+    {
+        std::cout << "Install glfw3"<< std::endl;
+    }
+
+    while(1)
+        GLFWwindow* window = glfwCreateWindow(640, 480, "My Title", NULL, NULL);
+
+    glfwTerminate();
     return 0;
 }
