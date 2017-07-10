e2cfdeb437818e6a7c76e7361415bacd8a49af4b
trent <tjmcc01@gmail.com>
2017-07-09 21:52:24 -0700

    Adds instruction information to cpu

--- nes/cpu.cpp
+++ nes/cpu.cpp
@@ -1,4 +1,79 @@
+#include <array>
 #include "include/cpu.h"
+
+std::array<uint8_t, 256> instr_modes = {
+        6, 7, 6, 7, 11, 11, 11, 11, 6, 5, 4, 5, 1, 1, 1, 1,
+        10, 9, 6, 9, 12, 12, 12, 12, 6, 3, 6, 3, 2, 2, 2, 2,
+        1, 7, 6, 7, 11, 11, 11, 11, 6, 5, 4, 5, 1, 1, 1, 1,
+        10, 9, 6, 9, 12, 12, 12, 12, 6, 3, 6, 3, 2, 2, 2, 2,
+        6, 7, 6, 7, 11, 11, 11, 11, 6, 5, 4, 5, 1, 1, 1, 1,
+        10, 9, 6, 9, 12, 12, 12, 12, 6, 3, 6, 3, 2, 2, 2, 2,
+        6, 7, 6, 7, 11, 11, 11, 11, 6, 5, 4, 5, 8, 1, 1, 1,
+        10, 9, 6, 9, 12, 12, 12, 12, 6, 3, 6, 3, 2, 2, 2, 2,
+        5, 7, 5, 7, 11, 11, 11, 11, 6, 5, 6, 5, 1, 1, 1, 1,
+        10, 9, 6, 9, 12, 12, 13, 13, 6, 3, 6, 3, 2, 2, 3, 3,
+        5, 7, 5, 7, 11, 11, 11, 11, 6, 5, 6, 5, 1, 1, 1, 1,
+        10, 9, 6, 9, 12, 12, 13, 13, 6, 3, 6, 3, 2, 2, 3, 3,
+        5, 7, 5, 7, 11, 11, 11, 11, 6, 5, 6, 5, 1, 1, 1, 1,
+        10, 9, 6, 9, 12, 12, 12, 12, 6, 3, 6, 3, 2, 2, 2, 2,
+        5, 7, 5, 7, 11, 11, 11, 11, 6, 5, 6, 5, 1, 1, 1, 1,
+        10, 9, 6, 9, 12, 12, 12, 12, 6, 3, 6, 3, 2, 2, 2, 2,
+};
+
+std::array<uint8_t, 256> instr_cycles = {
+        1, 2, 0, 0, 2, 2, 2, 0, 1, 2, 1, 0, 3, 3, 3, 0,
+        2, 2, 0, 0, 2, 2, 2, 0, 1, 3, 1, 0, 3, 3, 3, 0,
+        3, 2, 0, 0, 2, 2, 2, 0, 1, 2, 1, 0, 3, 3, 3, 0,
+        2, 2, 0, 0, 2, 2, 2, 0, 1, 3, 1, 0, 3, 3, 3, 0,
+        1, 2, 0, 0, 2, 2, 2, 0, 1, 2, 1, 0, 3, 3, 3, 0,
+        2, 2, 0, 0, 2, 2, 2, 0, 1, 3, 1, 0, 3, 3, 3, 0,
+        1, 2, 0, 0, 2, 2, 2, 0, 1, 2, 1, 0, 3, 3, 3, 0,
+        2, 2, 0, 0, 2, 2, 2, 0, 1, 3, 1, 0, 3, 3, 3, 0,
+        2, 2, 0, 0, 2, 2, 2, 0, 1, 0, 1, 0, 3, 3, 3, 0,
+        2, 2, 0, 0, 2, 2, 2, 0, 1, 3, 1, 0, 0, 3, 0, 0,
+        2, 2, 2, 0, 2, 2, 2, 0, 1, 2, 1, 0, 3, 3, 3, 0,
+        2, 2, 0, 0, 2, 2, 2, 0, 1, 3, 1, 0, 3, 3, 3, 0,
+        2, 2, 0, 0, 2, 2, 2, 0, 1, 2, 1, 0, 3, 3, 3, 0,
+        2, 2, 0, 0, 2, 2, 2, 0, 1, 3, 1, 0, 3, 3, 3, 0,
+        2, 2, 0, 0, 2, 2, 2, 0, 1, 2, 1, 0, 3, 3, 3, 0,
+        2, 2, 0, 0, 2, 2, 2, 0, 1, 3, 1, 0, 3, 3, 3, 0,
+};
+
+std::array<std::string, 256> instr_names = {
+        "BRK", "ORA", "KIL", "SLO", "NOP", "ORA", "ASL", "SLO",
+        "PHP", "ORA", "ASL", "ANC", "NOP", "ORA", "ASL", "SLO",
+        "BPL", "ORA", "KIL", "SLO", "NOP", "ORA", "ASL", "SLO",
+        "CLC", "ORA", "NOP", "SLO", "NOP", "ORA", "ASL", "SLO",
+        "JSR", "AND", "KIL", "RLA", "BIT", "AND", "ROL", "RLA",
+        "PLP", "AND", "ROL", "ANC", "BIT", "AND", "ROL", "RLA",
+        "BMI", "AND", "KIL", "RLA", "NOP", "AND", "ROL", "RLA",
+        "SEC", "AND", "NOP", "RLA", "NOP", "AND", "ROL", "RLA",
+        "RTI", "EOR", "KIL", "SRE", "NOP", "EOR", "LSR", "SRE",
+        "PHA", "EOR", "LSR", "ALR", "JMP", "EOR", "LSR", "SRE",
+        "BVC", "EOR", "KIL", "SRE", "NOP", "EOR", "LSR", "SRE",
+        "CLI", "EOR", "NOP", "SRE", "NOP", "EOR", "LSR", "SRE",
+        "RTS", "ADC", "KIL", "RRA", "NOP", "ADC", "ROR", "RRA",
+        "PLA", "ADC", "ROR", "ARR", "JMP", "ADC", "ROR", "RRA",
+        "BVS", "ADC", "KIL", "RRA", "NOP", "ADC", "ROR", "RRA",
+        "SEI", "ADC", "NOP", "RRA", "NOP", "ADC", "ROR", "RRA",
+        "NOP", "STA", "NOP", "SAX", "STY", "STA", "STX", "SAX",
+        "DEY", "NOP", "TXA", "XAA", "STY", "STA", "STX", "SAX",
+        "BCC", "STA", "KIL", "AHX", "STY", "STA", "STX", "SAX",
+        "TYA", "STA", "TXS", "TAS", "SHY", "STA", "SHX", "AHX",
+        "LDY", "LDA", "LDX", "LAX", "LDY", "LDA", "LDX", "LAX",
+        "TAY", "LDA", "TAX", "LAX", "LDY", "LDA", "LDX", "LAX",
+        "BCS", "LDA", "KIL", "LAX", "LDY", "LDA", "LDX", "LAX",
+        "CLV", "LDA", "TSX", "LAS", "LDY", "LDA", "LDX", "LAX",
+        "CPY", "CMP", "NOP", "DCP", "CPY", "CMP", "DEC", "DCP",
+        "INY", "CMP", "DEX", "AXS", "CPY", "CMP", "DEC", "DCP",
+        "BNE", "CMP", "KIL", "DCP", "NOP", "CMP", "DEC", "DCP",
+        "CLD", "CMP", "NOP", "DCP", "NOP", "CMP", "DEC", "DCP",
+        "CPX", "SBC", "NOP", "ISC", "CPX", "SBC", "INC", "ISC",
+        "INX", "SBC", "NOP", "SBC", "CPX", "SBC", "INC", "ISC",
+        "BEQ", "SBC", "KIL", "ISC", "NOP", "SBC", "INC", "ISC",
+        "SED", "SBC", "NOP", "ISC", "NOP", "SBC", "INC", "ISC",
+};
+
 
 cpu::cpu() : cycles(0), PC(0), SP(0), A(0), X(0), Y(0), flags(0), interrupt(0), stall(0) {}
 
