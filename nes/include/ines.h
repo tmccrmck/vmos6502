cf3b3aa469af4ec1b5f1191715e1ec66cc46cf3a
trent <tjmcc01@gmail.com>
2018-02-05 08:11:23 -0800

    Got it compiling

--- nes/include/ines.h
+++
@@ -1,3 +0,0 @@
-#include "rom.h"
-
-rom load_nes(const char *path);
--- nes/rom.cpp
+++
@@ -1,7 +0,0 @@
-#include "include/rom.h"
-
-rom::rom(ines_header h, std::vector<uint8_t> prg_rom, std::vector<uint8_t> chr_rom) {
-    header = h;
-    prg = prg_rom;
-    chr = chr_rom;
-}
